87bfe0e2f4305b7b203077ec9884b0b8ecd35960
Arseniy Pelevin <arseniy.pelevin@gmail.com>
2022-06-16 01:17:19 +0300

    func calcDistance

--- OpenFields processing.py
+++ OpenFields processing.py
@@ -13,44 +13,53 @@ class Statistics():
         df.columns.values[0] = "Time"      
         self.totalTime = self.calcTotalTime(df)
         self.totalDistance = self.calcDistance(df)
-        self.totalVelocity = self.calcTotalVelocity(df)
+        self.totalVelocity = self.totalDistance / self.totalTime
         self.rearings = self.calcRearings(df)
-      #  print(df.to_string())
+#        print(df.to_string())
+       # print(df)
 
     def calcTotalTime(self, df):
-        timeStart = pd.Timestamp(df.loc[df.index[0], 'Time'])
-        timeEnd = pd.Timestamp(df.loc[df.index[-1], 'Time'])
+        timeStart = pd.Timestamp(df.at[0, 'Time'])
+        timeEnd = pd.Timestamp(df.at[df.index[-1], 'Time'])
         totalTimeSec = pd.Interval(timeStart, timeEnd).length.seconds
         totalTimeMicrSec = pd.Interval(timeStart, timeEnd).length.microseconds
-        totalTime = f"{totalTimeSec}.{totalTimeMicrSec}"
+        totalTime = float(f"{totalTimeSec}.{totalTimeMicrSec}")
         print(f'Total time: {totalTime}')
         return totalTime
         
+    # distance in cell units yet
     def calcDistance(self, df):
         df['X'] = 0
         df['Y'] = 0
         df['dist'] = 0
         totalDistance = 0
         for i, row in df.iterrows():
+            
+            # calculate central point of animal
             df.at[i, 'X'] = st.fmean([df.at[i, 'X1'], df.at[i, 'X2']])
             df.at[i, 'Y'] = st.fmean([df.at[i, 'Y1'], df.at[i, 'Y2']])
-            if i != 0:
-                p = [df.at[i-1, 'X'], df.at[i-1, 'Y']]   # previous point
-                q = [df.at[i, 'X'], df.at[i, 'Y']]          # current point
-              #  print(p, q)
-                dist = m.dist(p, q)
-                df.at[i, 'dist'] = dist
-                totalDistance += dist
-        print(f'Total distance: {totalDistance}')
-      #  print(df.to_string())
-        return totalDistance
+            
+            if i == 0: continue
+            p = [df.at[i-1, 'X'], df.at[i-1, 'Y']]   # previous point
+            q = [df.at[i, 'X'], df.at[i, 'Y']]       # current point
+            dist = m.dist(p, q)
+            df.at[i, 'dist'] = dist                  # dist since last point
+            totalDistance += dist
                 
-        
-    def calcTotalVelocity(self, df):
-        pass
+        print(f'Total distance: {totalDistance}')
+        return totalDistance
         
     def calcRearings(self, df):
-        pass
+        wasRearing = False
+        rearings = 0
+        for i, row in df.iterrows():
+            if row['Z'] and not wasRearing:
+                rearings += 1
+                wasRearing = True
+            elif not row['Z'] and wasRearing:
+                wasRearing = False
+        print(f'Rearings: {rearings}')
+        return rearings
         
 
 class MainWindow(QMainWindow):
