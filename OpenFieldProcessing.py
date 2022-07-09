a0f9dcce7633b2537ef1959e5387065ecd85ddb8
Arseniy Pelevin <arseniy.pelevin@gmail.com>
2022-07-10 00:55:10 +0300

    Intermadiate, many commented attempts to fix a bug
    Map buttons would only update after second click on area buttons. Commented here are many
    failed attemts to fix it.
    Finally going to use it as feature: area buttons should be checkable

--- OpenFieldProcessing.py
+++ OpenFieldProcessing.py
@@ -5,6 +5,7 @@
 #from PyQt6.QtCore import QEvent
 #from PyQt6.QtGui import QFontMetrics, QFont
 
+
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
@@ -14,8 +15,11 @@ import pandas as pd
 
 from OpenFieldStatistics import OFStatistics
 
-import sys
+import os
 import re
+
+import ctypes
+import sip
      
 
 class MainWindow(QMainWindow):
@@ -26,6 +30,14 @@ class MainWindow(QMainWindow):
         
         self.numLasers = 16
         self.mapSide = 320 # px
+        
+        print(os.sys.version)
+        
+        generalLayout = QHBoxLayout()
+        controlLayout = QGridLayout()
+        timeRangeLayout = QGridLayout()
+        dataLayout = QVBoxLayout()
+        self.areaButtonLayout = QVBoxLayout()
         
         self.getFileButton = QPushButton("Select file")
         self.showStatButton = QPushButton("Show general statistics")
@@ -53,9 +65,20 @@ class MainWindow(QMainWindow):
         self.timeRangeSlider.setDisabled(True)
         
         self.map = QLabel()
-        # self.squareMapButtons()
-        # self.verticalMapButtons()
-        self.horizontalMapButtons()
+        self.drawMap()
+        self.areaButtons()
+        
+        # self.checkedAreaBtn = 'Cell'
+        # self.cellMapButtons()
+        self.areaBtn['Cell'].toggled.connect(self.cellMapButton)
+        self.areaBtn['Vertical_halves'].toggled.connect(self.vHalfMapArea)
+        self.areaBtn['Horizontal_halves'].toggled.connect(self.hHalfMapArea)
+        self.areaBtn['Wall'].toggled.connect(self.wallMapArea)        
+        self.areaBtn['Column'].toggled.connect(self.columnMapButtons)
+        self.areaBtn['Row'].toggled.connect(self.verticalMapButtons)
+        self.areaBtn['Square'].toggled.connect(self.horizontalMapButtons)
+
+
                 
         self.getFileButton.clicked.connect(self.getFile)
         # Check if input is correct
@@ -67,22 +90,17 @@ class MainWindow(QMainWindow):
         self.endTime.editingFinished.connect(self.textUpdateTimeRange)
         # Update time range from slider
         self.timeRangeSlider.sliderMoved.connect(self.updateMap)
-        self.timeRangeSlider.sliderReleased.connect(self.sliderUpdateTimeRange)  
+        self.timeRangeSlider.sliderReleased.connect(self.sliderUpdateTimeRange)        
+               
+        timeRangeLayout.addWidget(self.startTime, 0, 0, Qt.AlignLeft)
+        timeRangeLayout.addWidget(self.endTime, 0, 1, Qt.AlignRight)
+        timeRangeLayout.addWidget(self.timeRangeSlider, 1, 0, 1, 2, Qt.AlignBottom)
         
-        generalLayout = QHBoxLayout()
-        controlLayout = QVBoxLayout()
-        timeRangeLayout = QGridLayout()
-        dataLayout = QVBoxLayout()
-        
-        timeRangeLayout.addWidget(self.startTime, 0, 0, alignment = Qt.AlignLeft)
-        timeRangeLayout.addWidget(self.endTime, 0, 1, alignment = Qt.AlignRight)
-        timeRangeLayout.addWidget(self.timeRangeSlider, 1, 0, 1, 2)
-        
-        controlLayout.addWidget(self.getFileButton)
-        controlLayout.addWidget(self.fileName, alignment = Qt.AlignTop)
-        # controlLayout.addWidget(self.selectTime, alignment = Qt.AlignBottom)
-        controlLayout.addWidget(self.map, alignment = Qt.AlignCenter)
-        controlLayout.addLayout(timeRangeLayout)
+        controlLayout.addWidget(self.getFileButton, 0, 0, 1, 2)
+        controlLayout.addWidget(self.fileName, 1, 0, 1, 2, Qt.AlignTop)
+        controlLayout.addLayout(self.areaButtonLayout, 2, 0, 1, 1, Qt.AlignLeft)
+        controlLayout.addWidget(self.map, 2, 1, 1, 1, Qt.AlignHCenter)
+        controlLayout.addLayout(timeRangeLayout, 3, 0, 1, 2, Qt.AlignBottom)
         
         dataLayout.addWidget(self.totalTime)
         dataLayout.addWidget(self.totalDistance)
@@ -113,7 +131,7 @@ class MainWindow(QMainWindow):
         self.stat = OFStatistics(self.df) 
         self.totalStat()
         self.setTimeRange()
-        self.drawMap(0, self.df.index[-1])
+        self.drawPath(0, self.df.index[-1])
         
     def totalStat(self):
         self.totalTime.setText(f"Total time: {self.stat.totalTime} s")
@@ -136,7 +154,7 @@ class MainWindow(QMainWindow):
         selectedRearings = self.stat.calcRearings(self.df, iStart, iEnd)
         self.selectedRearings.setText(f"Selected rearings: {selectedRearings}")
         
-        self.drawMap(iStart, iEnd)
+        self.drawPath(iStart, iEnd)
         
     def setTimeRange(self):
         # slider is sensitive to 0.1 sec
@@ -189,11 +207,9 @@ class MainWindow(QMainWindow):
         start, end = [x/10 for x in self.timeRangeSlider.value()]
         iStart = self.stat.timePoint(self.df, start)
         iEnd = self.stat.timePoint(self.df, end)
-        self.drawMap(iStart, iEnd)
+        self.drawPath(iStart, iEnd)
                 
-    def drawMap(self, iStart, iEnd):
-        
-        
+    def drawMap(self):
         canvas = QPixmap(self.mapSide + 1, self.mapSide + 1)
         canvas.fill()
         painter = QPainter(canvas)
@@ -205,7 +221,13 @@ class MainWindow(QMainWindow):
             painter.drawLine(0, step, self.mapSide, step)
             painter.drawLine(step, 0, step, self.mapSide)
             
-        # Draw path
+        painter.end()
+        self.map.setPixmap(canvas) 
+        return cell, canvas
+            
+    def drawPath(self, iStart, iEnd):
+        cell, canvas = self.drawMap()
+        painter = QPainter(canvas)
         painter.setPen(QPen(Qt.red, 2))
         lastX, lastY = 0, 0
         for i, row in self.df.iloc[iStart:iEnd, :].iterrows():
@@ -221,56 +243,181 @@ class MainWindow(QMainWindow):
                 lastY = y
            
         painter.end()
-        self.map.setPixmap(canvas)      
+        self.map.setPixmap(canvas)   
         
-    def squareMapButtons(self):
-        self.mapLayout = QGridLayout(self.map)
-        self.mapLayout.setSpacing(0)
-        self.mapLayout.setContentsMargins(1, 1, 1, 1)
-        cell = int(self.mapSide / self.numLasers) # px
-        mapButtons = []
-        for i in range(self.numLasers):
-            mapButtons.append([])
-            for j in range(self.numLasers):
-                mapButtons[i].append(QPushButton('', self.map))
-                mapButtons[i][j].setFixedSize(cell, cell)
-                mapButtons[i][j].setCheckable(True)
-                self.mapLayout.addWidget(mapButtons[i][j], i, j)
-        
+    def newAreaBtn(self, name, times):
+        print('a')
+        if hasattr(self, 'mapLayout'):
+            print(self.mapLayout)
+       # self.areaBtn[name].setChecked(True)
+        #self.areaBtn[self.checkedAreaBtn].setChecked(False)
+        self.checkedAreaBtn = name
+        if hasattr(self, 'mapLayout'):
+            print('b')
+            if hasattr(self, 'mapLayout'):
+                print(self.mapLayout)
+            for i in reversed(range(self.mapLayout.count())):
+                item = self.mapLayout.itemAt(i).widget()
+                #item.setParent(None)         # We want all previous buttons deleted
+                item.deleteLater()
+                # sip.delete(item)
+             
+
+          #  print(self.mapLayout, self.mapLayout.count())
+        # if hasattr(self, 'mapButtons'):
+        #     print('was here!!')
+        #     for item in self.mapButtons:
+        #         del item
+            # self.mapLayout.deleteLater()
+            # self.map.itemAt(0).removeItem(self.mapLayout)
+            #sip.delete(self.mapLayout)
+            # sip.delete(self.mapLayout)
+
+            self.mapLayout.deleteLater()
+            # QCoreApplication.processEvents()
+            
+            
+           # print(self.mapLayout, self.mapLayout.count())
+        # self.mapButtons = []
+        # cell = int(self.mapSide / self.numLasers) # px
         with open('ButtonStyleSheet.css') as buttonStyleSheet:
             self.map.setStyleSheet(buttonStyleSheet.read())
+        # return cell
+        
+        # if times == 0:
+        #     if name == 'Cell': self.cellMapButtons(times)
+        #     if name == 'Column': self.verticalMapButtons(times)
+        #     if name == 'Row': self.horizontalMapButtons(times)
+            
+    def cellMapButtons(self, times):
+        # print('CELL 1')
+        # if hasattr(self, 'mapLayout'):
+        #     print(self.mapLayout)
+            
+        # if times == 1: self.newAreaBtn('Cell', times)   
+        
+        # print('CELL 2')
+        # if hasattr(self, 'mapLayout'):
+        #     print(self.mapLayout)
+           
+        cell = int(self.mapSide / self.numLasers) # px
+        mapCButtons = []
+        
+        self.mapLayout = QGridLayout(self.map)
+        self.mapLayout.setSpacing(0)
+        self.mapLayout.setContentsMargins(0, 0, 0, 0)
+        
+        for i in range(self.numLasers):
+            mapCButtons.append([])
+            for j in range(self.numLasers):
+                mapCButtons[i].append(QPushButton('', self.map))
+                mapCButtons[i][j].setFixedSize(cell, cell)
+                mapCButtons[i][j].setCheckable(True)
+                self.mapLayout.addWidget(mapCButtons[i][j], i, j)
 
-    def verticalMapButtons(self):
+        
+
+            #self.cellMapButtons()
+        
+        # with open('ButtonStyleSheet.css') as buttonStyleSheet:
+        #     self.map.setStyleSheet(buttonStyleSheet.read())
+
+    def verticalMapButtons(self, times):
+        # print('COLUMN 1')
+        # if hasattr(self, 'mapLayout'):
+        #     print(self.mapLayout)
+            
+        # cell = self.newAreaBtn('Column')  
+        
+        # print('COLUMN 2')
+        # if hasattr(self, 'mapLayout'):
+        #     print(self.mapLayout)
+        
         self.mapLayout = QHBoxLayout(self.map)
         self.mapLayout.setSpacing(0)
         self.mapLayout.setContentsMargins(1, 1, 1, 1)
-        cell = int(self.mapSide / self.numLasers) # px
-        mapButtons = []
-        for i in range(self.numLasers):
-                mapButtons.append(QPushButton('', self.map))
-                mapButtons[i].setFixedSize(cell, self.mapSide)
-                mapButtons[i].setCheckable(True)
-                self.mapLayout.addWidget(mapButtons[i])
         
-        with open('ButtonStyleSheet.css') as buttonStyleSheet:
-            self.map.setStyleSheet(buttonStyleSheet.read())
+        cell = int(self.mapSide / self.numLasers) # px
+        mapVButtons = []
+        
+        for i in range(self.numLasers):
+            mapVButtons.append(QPushButton('', self.map))
+            mapVButtons[i].setFixedSize(cell, self.mapSide)
+            mapVButtons[i].setCheckable(True)
+            self.mapLayout.addWidget(mapVButtons[i])
             
-    def horizontalMapButtons(self):
+        print('Column', self.mapLayout, self.mapLayout.count())
+        
+        # if self.timesNewButton == 0:
+        #     self.timesNewButton = 1
+        #     self.newAreaBtn('Column')
+        #     self.verticalMapButtons()
+                
+        # with open('ButtonStyleSheet.css') as buttonStyleSheet:
+        #     self.map.setStyleSheet(buttonStyleSheet.read())
+        
+        # times += 1
+        # if times < 2:
+        #     self.newAreaBtn('Column', times)
+            #self.cellMapButtons()
+            
+    def horizontalMapButtons(self):      
+        # cell = self.newAreaBtn('Row')
+        
         self.mapLayout = QVBoxLayout(self.map)
         self.mapLayout.setSpacing(0)
         self.mapLayout.setContentsMargins(1, 1, 1, 1)
-        cell = int(self.mapSide / self.numLasers) # px
-        mapButtons = []
-        for i in range(self.numLasers):
-                mapButtons.append(QPushButton('', self.map))
-                mapButtons[i].setFixedSize(self.mapSide, cell)
-                mapButtons[i].setCheckable(True)
-                self.mapLayout.addWidget(mapButtons[i])
         
-        with open('ButtonStyleSheet.css') as buttonStyleSheet:
-            self.map.setStyleSheet(buttonStyleSheet.read())
+        cell = int(self.mapSide / self.numLasers) # px
+        mapHButtons = []
+        
+        for i in range(self.numLasers):
+            mapHButtons.append(QPushButton('', self.map))
+            mapHButtons[i].setFixedSize(self.mapSide, cell)
+            mapHButtons[i].setCheckable(True)
+            self.mapLayout.addWidget(mapHButtons[i])
             
-app = QApplication(sys.argv)
+        print('Row', self.mapLayout, self.mapLayout.count())
+        
+        # with open('ButtonStyleSheet.css') as buttonStyleSheet:
+        #     self.map.setStyleSheet(buttonStyleSheet.read())
+            
+    def areaButtons(self):
+        self.areaBtnNames = ['Cell', 'Vertical_halves', 'Horizontal_halves',
+                             'Wall', 'Column', 'Row', 'Square']
+        self.areaBtn = {}
+        numAreaBtn = 7
+        for i in range(numAreaBtn):
+            name = self.areaBtnNames[i]
+            self.areaBtn[name] = QPushButton()
+            
+            pixmap = QPixmap(os.path.join('Area_Buttons_pixmaps', 
+                                  self.areaBtnNames[i] + '.png'))
+            # palette = QPalette()
+            # palette.setBrush(self.areaBtn[name].backgroundRole(), QBrush(pixmap))
+
+            # self.areaBtn[name].setPalette(palette)
+            # self.areaBtn[name].setStyleSheet(f'''
+            #         background-image: 
+            #     url({os.path.join('Area_Buttons_pixmaps', self.areaBtnNames[i] + '.png')});
+                   
+            #         ''')
+            # self.areaBtn[name].setIcon(QIcon(os.path.join('Area_Buttons_pixmaps', 
+            #                                          self.areaBtnNames[i] + '.png')))
+            self.areaBtn[name].setIcon(QIcon(pixmap))
+          ##  self.areaBtn[name].setFlat(True)
+           # self.areaBtn[name].setAutoFillBackground(True)
+        #    self.areaBtn[name].setAttribute(Qt.WA_TranslucentBackground)
+            self.areaBtn[name].setIconSize(QSize(30, 30))
+            self.areaBtn[name].setFixedSize(30, 30)
+            self.areaButtonLayout.addWidget(self.areaBtn[name])
+            self.areaBtn[name].setCheckable(True)
+            # print(name)
+        self.areaButtonLayout.setSpacing(int((self.mapSide - (30 * numAreaBtn)) / \
+                                         (numAreaBtn - 1)))
+        self.areaButtonLayout.setContentsMargins(0, 0, 30, 0)
+            
+app = QApplication(os.sys.argv)
 window = MainWindow()
 window.show()
 app.exec()
