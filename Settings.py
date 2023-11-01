8fa13fed9aa8c67c032a44f8dd5eda15fd0d03d9
Arseniy Pelevin <arseniy.pelevin@gmail.com>
2023-11-01 22:36:08 +0300

    Do not autosave time and zone parameters as recent settings

--- Settings.py
+++ Settings.py
@@ -24,12 +24,12 @@ from PyQt6.QtGui import (
 from superqt import QRangeSlider
 
 ALL_STAT_PARAMS = ['time', 'dist', 'velocity', 'rearing_n', 'rearing_time']
-DEFAULT_PARAMS = {
+DEFAULT_SETTINGS = {
     'numLasersX': 16,
     'numLasersY': 16,
     'boxSideX': 40.,      # Physical dimensions of the filed, cm
     'boxSideY': 40.,
-    'statParams': set(ALL_STAT_PARAMS),
+    'statParams': ALL_STAT_PARAMS,
     'samplingFrequency': 0.1,
     'startTime': 1,  # First beam break
     'separator': ';',
@@ -47,24 +47,26 @@ class Settings(QDialog):
         self.loadParameters()
 
 
-#TODO QTabBar
+#TODO QTabBar ?
 #https://doc.qt.io/qt-5/qtabwidget.html
 
     def loadParameters(self):
+        print(__name__, inspect.currentframe().f_code.co_name)
+
         if 'RecentParameters.json' in os.listdir():
             with open('RecentParameters.json', 'r', newline='') as file:
-                self.params = json.load(file)
-                self.params['statParams'] = set(self.params['statParams'])
+                self.settings = json.load(file)
         else:
-            self.params = DEFAULT_PARAMS
+            self.settings = DEFAULT_SETTINGS
 
-        self.window.params = self.params
+        self.window.params.update(self.settings)
+
 
 
     def settingsDialog(self):
         print(__name__, inspect.currentframe().f_code.co_name)
 
-        self.tempParams = self.params.copy()
+        self.tempSettings = self.settings.copy()
 
         self.setWindowTitle('Settings')
 
@@ -84,64 +86,63 @@ class Settings(QDialog):
         self.layout.addWidget(self.buttonBox)
 
         if self.exec():
-            self.updateParameters()
+            self.settings = self.tempSettings.copy()
+            self.window.params.update(self.settings)
+            self.saveRecentParameters()
 
-    def updateParameters(self):
-        #TODO statParams from booleans to Series
-
-        self.params = self.tempParams.copy() #OK
-
-        self.tempParams['statParams'] = list(self.tempParams['statParams'])
+    def saveRecentParameters(self):
+        print(__name__, inspect.currentframe().f_code.co_name)
 
         with open('RecentParameters.json', 'w+', newline='') as file:
-            json.dump(self.tempParams, file, indent='\t')
+            json.dump(self.settings, file, indent='\t')
 
     def createFileGroup(self):
+        print(__name__, inspect.currentframe().f_code.co_name)
+
         fileGroup = QGroupBox('Default files locations')
         fileGroupLayout = QGridLayout(fileGroup)
 
-
         return fileGroup
 
-
-
     def createFieldParametersGroup(self):
+        print(__name__, inspect.currentframe().f_code.co_name)
+
         fieldParametersGroup = QGroupBox('Field parameters')
         fieldParametersGroupLayout = QGridLayout(fieldParametersGroup)
 
         numLasersXLabel = QLabel('Number of beams by horizontal axis')
         numLasersX = QSpinBox()
         numLasersX.setRange(1, 999) #TODO Test 1
-        numLasersX.setValue(self.tempParams['numLasersX'])
+        numLasersX.setValue(self.tempSettings['numLasersX'])
 
 
         numLasersYLabel = QLabel('Number of beams by vertical axis')
         numLasersY = QSpinBox()
         numLasersY.setRange(1, 999)
-        numLasersY.setValue(self.tempParams['numLasersY'])
+        numLasersY.setValue(self.tempSettings['numLasersY'])
 
         boxSideXLabel = QLabel('Horizontal field size (cm)')
         boxSideX = QDoubleSpinBox()
         boxSideX.setRange(1., 999.)
-        boxSideX.setDecimals(2)
+        boxSideX.setDecimals(1)
         boxSideX.setSuffix(' cm')
-        boxSideX.setValue(self.tempParams['boxSideX'])
+        boxSideX.setValue(self.tempSettings['boxSideX'])
 
         boxSideYLabel = QLabel('Vertical field size (cm)')
         boxSideY = QDoubleSpinBox()
         boxSideY.setRange(1., 999.)
-        boxSideY.setDecimals(2)
+        boxSideY.setDecimals(1)
         boxSideY.setSuffix(' cm')
-        boxSideY.setValue(self.tempParams['boxSideY'])
+        boxSideY.setValue(self.tempSettings['boxSideY'])
 
         numLasersX.valueChanged.connect(lambda val:
-            self.tempParams.update({'numLasersX': val}))
+            self.tempSettings.update({'numLasersX': val}))
         numLasersY.valueChanged.connect(lambda val:
-            self.tempParams.update({'numLasersY': val}))
+            self.tempSettings.update({'numLasersY': val}))
         boxSideX.valueChanged.connect(lambda val:
-            self.tempParams.update({'boxSideX': val}))
+            self.tempSettings.update({'boxSideX': val}))
         boxSideY.valueChanged.connect(lambda val:
-            self.tempParams.update({'boxSideY': val}))
+            self.tempSettings.update({'boxSideY': val}))
 
         fieldParametersGroupLayout.addWidget(numLasersXLabel, 0, 0)
         fieldParametersGroupLayout.addWidget(numLasersX, 0, 1)
@@ -155,35 +156,37 @@ class Settings(QDialog):
         return fieldParametersGroup
 
     def createStatisticsGroup(self):
+        print(__name__, inspect.currentframe().f_code.co_name)
+
         statisticsGroup = QGroupBox('Output statistics')
         statisticsGroupLayout = QVBoxLayout(statisticsGroup)
 
         time = QCheckBox('Time')
-        time.setChecked('time' in self.tempParams['statParams'])
+        time.setChecked('time' in self.tempSettings['statParams'])
         dist = QCheckBox('Distance')
-        dist.setChecked('dist' in self.tempParams['statParams'])
+        dist.setChecked('dist' in self.tempSettings['statParams'])
         velocity = QCheckBox('Velocity')
-        velocity.setChecked('velocity' in self.tempParams['statParams'])
+        velocity.setChecked('velocity' in self.tempSettings['statParams'])
         rearingN = QCheckBox('Rearings number')
-        rearingN.setChecked('rearing_n' in self.tempParams['statParams'])
+        rearingN.setChecked('rearing_n' in self.tempSettings['statParams'])
         rearingTime = QCheckBox('Rearings time')
-        rearingTime.setChecked('rearing_time' in self.tempParams['statParams'])
+        rearingTime.setChecked('rearing_time' in self.tempSettings['statParams'])
 
         time.toggled.connect(lambda checked:
-            self.tempParams['statParams'].add('time') if checked
-            else self.tempParams['statParams'].remove('time'))
+            self.tempSettings['statParams'].append('time') if checked
+            else self.tempSettings['statParams'].remove('time'))
         dist.toggled.connect(lambda checked:
-            self.tempParams['statParams'].add('dist') if checked
-            else self.tempParams['statParams'].remove('dist'))
+            self.tempSettings['statParams'].append('dist') if checked
+            else self.tempSettings['statParams'].remove('dist'))
         velocity.toggled.connect(lambda checked:
-            self.tempParams['statParams'].add('velocity') if checked
-            else self.tempParams['statParams'].remove('velocity'))
+            self.tempSettings['statParams'].append('velocity') if checked
+            else self.tempSettings['statParams'].remove('velocity'))
         rearingN.toggled.connect(lambda checked:
-            self.tempParams['statParams'].add('rearing_n') if checked
-            else self.tempParams['statParams'].remove('rearing_n'))
+            self.tempSettings['statParams'].append('rearing_n') if checked
+            else self.tempSettings['statParams'].remove('rearing_n'))
         rearingTime.toggled.connect(lambda checked:
-            self.tempParams['statParams'].add('rearing_time') if checked
-            else self.tempParams['statParams'].remove('rearing_time'))
+            self.tempSettings['statParams'].append('rearing_time') if checked
+            else self.tempSettings['statParams'].remove('rearing_time'))
 
         statisticsGroupLayout.addWidget(time)
         statisticsGroupLayout.addWidget(dist)
@@ -194,6 +197,8 @@ class Settings(QDialog):
         return statisticsGroup
 
     def createSamplingGroup(self):
+        print(__name__, inspect.currentframe().f_code.co_name)
+
         samplingGroup = QGroupBox('Sampling parameters')
         samplingGroupLayout = QGridLayout(samplingGroup)
 
@@ -202,17 +207,17 @@ class Settings(QDialog):
         samplingFrequency.setRange(0.1, 1)
         samplingFrequency.setSingleStep(0.1)
         samplingFrequency.setSuffix(' s')
-        samplingFrequency.setValue(self.tempParams['samplingFrequency'])
+        samplingFrequency.setValue(self.tempSettings['samplingFrequency'])
 
         startTimeLabel = QLabel('Start time from: ')
         startTime = QComboBox()
         startTime.addItems(['Start of recording', 'First beam break'])
-        startTime.setCurrentIndex(self.tempParams['startTime'])
+        startTime.setCurrentIndex(self.tempSettings['startTime'])
 
         samplingFrequency.valueChanged.connect(lambda val:
-           self.tempParams.update({'samplingFrequency': val}))
+           self.tempSettings.update({'samplingFrequency': val}))
         startTime.currentIndexChanged.connect(lambda val:
-           self.tempParams.update({'startTime': val}))
+           self.tempSettings.update({'startTime': val}))
 
         samplingGroupLayout.addWidget(samplingFrequencyLabel, 0, 0)
         samplingGroupLayout.addWidget(samplingFrequency, 0, 1)
@@ -222,6 +227,8 @@ class Settings(QDialog):
         return samplingGroup
 
     def createOutputFormatGroup(self):
+        print(__name__, inspect.currentframe().f_code.co_name)
+
         outputFormatGroup = QGroupBox('Output format')
         outputFormatGroupLayout = QGridLayout(outputFormatGroup)
 
@@ -229,18 +236,18 @@ class Settings(QDialog):
         separator = QLineEdit()
         separator.setMaxLength(1)
         separator.setFixedWidth(20)
-        separator.setText(self.tempParams['separator'])
+        separator.setText(self.tempSettings['separator'])
 
         decimalLabel = QLabel('Output decimal point')
         decimal = QLineEdit()
         decimal.setMaxLength(1)
         decimal.setFixedWidth(20)
-        decimal.setText(self.tempParams['decimal'])
+        decimal.setText(self.tempSettings['decimal'])
 
         separator.textEdited.connect(lambda val:
-            self.tempParams.update({'separator': val}))
+            self.tempSettings.update({'separator': val}))
         decimal.textEdited.connect(lambda val:
-            self.tempParams.update({'decimal': val}))
+            self.tempSettings.update({'decimal': val}))
 
         outputFormatGroupLayout.addWidget(separatorLabel, 0, 0)
         outputFormatGroupLayout.addWidget(separator, 0, 1)
